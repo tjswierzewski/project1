6987e0e326397731460c94d6bc553ab4d78528e0
tjswierzewski <tjswierzewski@gmail.com>
2022-09-11 13:32:42 -0400

    create socket

--- client.cpp
+++ client.cpp
@@ -1,8 +1,16 @@
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <stdio.h>
 #include <iostream>
 
 int main(int argc, char const *argv[])
 {
-
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    struct sockaddr server_addr;
+    if (sock < 0)
+    {
+        perror("Creating the socket had the following error\n");
+    }
+    printf("Hello from client\n");
     return 0;
 }
