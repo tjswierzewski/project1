6299b1b09339703688d36ae7bbd25f0117b190a4
tjswierzewski <tjswierzewski@gmail.com>
2022-09-11 17:32:21 -0400

    add connect

--- client.cpp
+++ client.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <string.h>
 #include <iostream>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 using namespace std;
 
@@ -11,17 +13,21 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    if (argc < 3 || argc > 5)
+    if (argc < 4 || argc > 6)
     {
         cout << "Usage: <-p port> <-s> [hostname] [Northeastern-username]" << endl;
     }
-    string username, hostname;
+    string username;
+    struct hostent *host = NULL;
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(sockaddr_in));
+    server_addr.sin_family = AF_INET;
     int encrypted = 0, port = PORT;
-    for (size_t i = 0; i < argc; i++)
+    for (size_t i = 1; i < argc; i++)
     {
         if (strcmp(argv[i], "-p") == 0)
         {
-            port = stoi(argv[i++]);
+            server_addr.sin_port = (short)stoi(argv[i++]);
         }
         else if (strcmp(argv[i], "-s") == 0)
         {
@@ -29,9 +35,15 @@ int main(int argc, char const *argv[])
         }
         else
         {
-            if (hostname.empty())
+            if (host == NULL)
             {
-                hostname = argv[i];
+                host = gethostbyname(argv[i]);
+                if (host == NULL)
+                {
+                    perror("Getting host by name caused the following error:\n");
+                    return -1;
+                }
+                server_addr.sin_addr = *(struct in_addr *)host->h_addr_list[0];
             }
             else
             {
@@ -40,14 +52,25 @@ int main(int argc, char const *argv[])
         }
     }
 
+    if (!encrypted)
+    {
+        cout << "Must be encrypted. Run with -s.";
+        return -1;
+    }
+
     int sock = socket(PF_INET, SOCK_STREAM, 0);
-    struct sockaddr server_addr;
     if (sock < 0)
     {
-        perror("Creating the socket had the following error\n");
+        perror("Creating the socket had the following error:\n");
+        return -1;
+    }
+    int client_fd = connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (client_fd < 0)
+    {
+        perror("Socket connection threw the following error\n");
+        return -1;
     }
 
     cout << "Hello from client" << endl;
-
     return 0;
 }
